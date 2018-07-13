1104a9709f139956c2b0579b7c9a4b36b53d4c9c
Jane <janekhl@yahoo.com>
2018-07-12 23:22:19 -0700

    functional

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # scrape-comment
+
+To use the app, add /scrape to the end of your link and then go back to the main page!
+
+Pros: Duplicates won't be published to the site and you can save your notes on each article
+Cons: the scrape button itself doesn't work but the route does
---
+++ server.js
@@ -0,0 +1,82 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 3000;
+
+var app = express();
+
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/ONIONSCRAPE");
+
+
+app.get("/scrape", function(req, res) {
+  axios.get("https://theonion.com/").then(function(response) {
+    var $ = cheerio.load(response.data);
+    $(".js_post_item").each(function(i, element) {
+      var result={};
+      result.link = $(element).children("header").children("h1").children("a").attr("href");
+      result.title = $(element).children("header").children("h1").children("a").text();
+      result.body = $(element).children(".js_item-content").children(".entry-summary").children("p").text();
+
+        db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          return res.json(err);
+        });
+    });
+    res.send("Scrape Complete");
+  });
+});
+
+app.get("/articles", function(req, res) {
+  db.Article.find({})
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.get("/articles/:id", function(req, res) {
+
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", function(req, res) {
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
